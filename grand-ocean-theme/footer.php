b8aaade29dd837f171af31f78919599c59f96e8e
Jun <junh4533@gmail.com>
2020-07-28 16:02:31 -0400

    add colors and footer (not done)

--- grand-ocean-theme/footer.php
+++ grand-ocean-theme/footer.php
@@ -1,8 +1,43 @@
 <!-- footer -->
 <footer id="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <div class="row">
+                    <div class="col-6">
+                        <i class="fas fa-map-marker-alt"></i> Google Maps
+                        <i class="fab fa-weixin"></i> WeChat
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT5btQn6gFp08LW8Vtqx728BKDWoDvI_OQ4Qg&usqp=CAU" alt="WeChat QR Code">
+                        <p>English</p> <p>中文</p>
+                    </div>
+                    <div class="col-6">
+                        <p>Site Links</p>
+                        <a href="">Home</a>
+                        <a href="">Products</a>
+                        <a href="">Contacts</a> 
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="row">
+                <div class="col-6">
+                        <p>Contact Information</p>
+                        <p>盛宴海鲜</p>
+                        <p>35-20 College Point <br> Blvd Flushing, NY <br> 11354</p>
+                        <p>Lisa Qiu</p>
+                        <p>Lisa307266</p>
+                        <p>(347) 532-0987</p>
+                    </div>
+                    <div class="col-6">
+                        <img id="footer-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-white.png" alt="Logo White">
+                        <p>©2014-2020 All Rights Reserved 盛宴海鲜</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
 
 <?php wp_footer();?>
-
 </body>
 </html>
