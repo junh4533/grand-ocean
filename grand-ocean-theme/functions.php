1022802039d6a37aed37f02e74efda15f618ae99
Nicholas Chen <grahmnic@gmail.com>
2020-07-28 16:01:50 -0400

    Adding woocommerce.php and custom woocommerce theme support.

--- grand-ocean-theme/functions.php
+++ grand-ocean-theme/functions.php
@@ -26,6 +26,26 @@ function load_scripts()
     wp_enqueue_script('custom-js');
 }
 
+function customtheme_add_woocommerce_support()
+{
+    add_theme_support( 'woocommerce' );
+}
+
 add_theme_support( 'post-thumbnails' );
+add_action( 'after_setup_theme', 'customtheme_add_woocommerce_support' );
 add_action('wp_enqueue_scripts', 'load_stylesheets');
 add_action('wp_enqueue_scripts', 'load_scripts');
+
+/**
+ * Override loop template and show quantities next to add to cart buttons
+ */
+add_filter( 'woocommerce_loop_add_to_cart_link', 'quantity_inputs_for_woocommerce_loop_add_to_cart_link', 10, 2 );
+function quantity_inputs_for_woocommerce_loop_add_to_cart_link( $html, $product ) {
+	if ( $product && $product->is_type( 'simple' ) && $product->is_purchasable() && $product->is_in_stock() && ! $product->is_sold_individually() ) {
+		$html = '<form action="' . esc_url( $product->add_to_cart_url() ) . '" class="cart" method="post" enctype="multipart/form-data">';
+		$html .= woocommerce_quantity_input( array(), $product, false );
+		$html .= '<button type="submit" class="button alt">' . esc_html( $product->add_to_cart_text() ) . '</button>';
+		$html .= '</form>';
+	}
+	return $html;
+}
---
+++ grand-ocean-theme/woocommerce.php
@@ -0,0 +1,7 @@
+<!-- ENSURE THAT THE TEMPLATE NAME IS CORRECT  -->
+<?php /* Template Name: Store */ ?>
+<?php get_header(); ?>
+    <div id="main">        
+        <?php echo do_shortcode("[products columns='4' class='products']"); ?>
+    </div>
+<?php get_footer(); ?>
