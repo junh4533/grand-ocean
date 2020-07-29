594aafae3cf7471fc86489b7373569045fed177d
Jun <junh4533@gmail.com>
2020-07-29 15:00:29 -0400

    add reservation settings

---
+++ grand-ocean-theme/page-reservation.php
@@ -0,0 +1,10 @@
+<!-- ENSURE THAT THE TEMPLATE NAME IS CORRECT  -->
+<?php /* Template Name: Reservation Page */ ?>
+
+<?php get_header();?>
+
+<section id="reservation">
+    
+</section>
+
+<?php get_footer();?>
