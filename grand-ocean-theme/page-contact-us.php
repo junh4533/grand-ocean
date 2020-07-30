c1dc3593c05b986ab537ceff8e3c8bc887c56315
Ivy Tong <ivytong@ivys-air.myfiosgateway.com>
2020-07-30 15:27:24 -0400

    Contact Us page

--- grand-ocean-theme/page-contact-us.php
+++ grand-ocean-theme/page-contact-us.php
@@ -1,5 +1,22 @@
 <!-- ENSURE THAT THE TEMPLATE NAME IS CORRECT  -->
 <?php /* Template Name: Contact Us */ ?>
 <?php get_header();?>
+    <section id="contact-us">
+        <div class="row">
+            <div class="col1">
+            <img src="/grand-ocean-theme/assets/images/seafood_place.jpg" alt="Exterior of Grand Ocean Seafood Wholesale Shop">
+            </div>
+            <div class="col2">
+            <h1>Contact Us</h1>
+            <div class=row>
+                <div class="col1">
+                </div>
+                <div class="col2">
+                    <h4>Lisa307266</h4>
+                    <h4>(347) 532-0987</h4>
+                </div>
+            </div>
+        <div>
 
+    </section>
 <?php get_footer();?>
