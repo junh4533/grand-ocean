79cc4342aaf63b84f42efa94e54f30f61ad3b7e8
minhdang1110 <nhatminhdang04@gmail.com>
2020-07-31 13:45:37 -0400

    adding testing confirmation

---
+++ grand-ocean-theme/page-confirmation.php
@@ -0,0 +1,34 @@
+<!-- ENSURE THAT THE TEMPLATE NAME IS CORRECT  -->
+<?php /* Template Name: Confirmation */ ?>
+<?php get_header();?>
+
+<section id="confirmation">
+    <div class="container">
+        <div class="column">
+            <p>Receipt Image</p>
+        </div>
+        
+        <div class="column">
+            <strong>You MUST call/WeChat message to CONFIRM the reservation</strong>
+            </br>
+            <strong>Otherwise your reservation will NOT be placed</strong>
+            </br>
+        
+            <div class=row>
+                <div class=col1>
+                    <a class="nav-link" href="<?php echo get_site_url(); ?>/#footer"><i class="fab fa-weixin"></i></a>
+                    <a class="nav-link" href="tel:347-532-0987"><i class="fas fa-phone"></i></a>
+                </div>
+                
+                <div class=col2>
+                    <h5>Lisa307266</h5>
+                    <br>
+                    <br>
+                    <h5>(347) 532-0987</h5>
+                </div>
+            </div>
+        </div>
+    <div>
+</section>
+
+<?php get_footer();?>
