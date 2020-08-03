4c579bba6d63cd2e1cd1e502f72aca3c9c50f686
Ivy Tong <ivytong@ivys-air.myfiosgateway.com>
2020-08-03 17:10:41 -0400

    Final about us page

--- grand-ocean-theme/front-page.php
+++ grand-ocean-theme/front-page.php
@@ -39,7 +39,18 @@
             <div class="column">
                 <h1>我们的故事</h1>
                 <br>
-                <p>We are an immigrant-owned seafood wholsale store located on College Point Blvd.</p>
+                <p>Grand Ocean is a seafood wholesale store based in College Point, NYC. We have been working diligently since 2015 to bring you the freshest seafood for the lowest price. Our wide selection of seafood is perfect for restaurants, seafood markets, small family gatherings, and catering large scale events. We will deliver your order in the most timely manner to ensure maximum freshness. Place your order now for fresh seafood!</p>
+                <br>
+                <h3>Hours of Operation</h3>
+                <br>
+                <br>
+                <p>We are open 10am-5pm every day of the week, including weekends! Operating hours may vary on holidays!</p>
+                <br>
+                <br>
+                <a class="nav-link" href="#products">
+                <button type="button" class="button1">Take me to the products!</button>
+                </a>
+            
             </div>
             <div class="column">
                 <div class="m-5">
