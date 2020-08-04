1d042093afc1f19d188b0a9a0dcff0d7f63c4724
minhdang1110 <nhatminhdang04@gmail.com>
2020-08-04 17:48:14 -0400

    fix conf message, add alert icon but not working yet

--- grand-ocean-theme/page-confirmation.php
+++ grand-ocean-theme/page-confirmation.php
@@ -2,6 +2,9 @@
 <?php /* Template Name: Confirmation */ ?>
 <?php get_header();?>
 
+<div id="alert">
+</div> 
+
 <section id="confirmation">
     <div class="container">
         <div class="row">
@@ -23,7 +26,46 @@
             </div>
             <div class="col-2"></div>
             <div class="col-6">
-                <div class="confirmation-message mb-3">
+                <div class="confirmation-message">
+                    <div id = step-0>
+                        <img src="/assets/images/alert.png" alt="alert icon"/>
+                        <h4>Reservation not yet completed!</h4>
+                    </div>
+                
+                    <div id='step-1'>
+                        <h5 id='step-title'>Step 1: </h5>
+                        <br>
+                        <h5>Contact us via the below phone number/WeChat:</h5>
+                        <br>
+                        <br>
+                        <a class="nav-link" href="<?php echo get_site_url(); ?>/#footer">
+                            <i class="fab fa-weixin" id='icon'></i>
+                            <h5>&nbsp;&nbsp;&nbsp;Lisa307266</h5>
+                        </a>
+                        <a class="nav-link" href="tel:347-532-0987">
+                            <i class="fas fa-phone" id='icon'></i>
+                            <h5>&nbsp;&nbsp;&nbsp;(347) 532-0987</h5>
+                        </a>
+                    </div>
+
+                    <div id='step-2'>
+                        <h5 id='step-title'>Step 2: </h5>
+                        <br>
+                        <h5>Confirm your order with us!</h5>
+                    </div>
+
+                    <div id='step-3'>
+                        <h5 id='step-title'>Step 3: </h5>
+                        <br>
+                        <h5>Wait for our delivery and enjoy the fresh seafood!</h5>
+                    </div>
+
+                    <div id='step-4'>
+                        <h6><em>* Failure of confirming will result in the cancellation of your order!</em></h6>
+                    </div>
+                </div>
+
+                <!-- <div class="confirmation-message mb-3">
                     <h4>You MUST call/WeChat message </br> to CONFIRM the reservation.</h4>
                     <br>
                     <br>
@@ -40,7 +82,7 @@
                         <i class="fas fa-phone" id='icon'></i>
                         <h5>&nbsp;&nbsp;&nbsp;(347) 532-0987</h5>
                     </a>
-                </div>
+                </div> -->
             </div>
         </div>
 </section>
