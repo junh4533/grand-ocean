268cd5bba2f7b5755611a2288e5b6d75678a4faf
minhdang1110 <nhatminhdang04@gmail.com>
2020-08-06 11:07:14 -0400

    responsive of confirmation

--- grand-ocean-theme/page-confirmation.php
+++ grand-ocean-theme/page-confirmation.php
@@ -5,7 +5,7 @@
 <section id="confirmation">
     <div class="container">
         <div class="row">
-            <div class="col-4" id='receipt'>
+            <div class="col-12 col-lg-5" id='receipt'>
                 <div>
                     <p>盛宴海鲜</p>
                     <p>35-20 College Point Blvd</p>
@@ -14,15 +14,15 @@
 
                 <div id="order-number-row">
                     <h3>订单号</br></h3>
-                    <h2><?php echo preg_replace('/[^0-9]+/' , '' , do_shortcode('[xlwcty_order]')) ?></h2>
+                    <!-- <h2><?php echo preg_replace('/[^0-9]+/' , '' , do_shortcode('[xlwcty_order]')) ?></h2> -->
                 </div>
 
                 <div>
-                    <h2><?php echo do_shortcode('[xlwcty_order_details]') ?></h2>
+                    <!-- <h2><?php echo do_shortcode('[xlwcty_order_details]') ?></h2> -->
                 </div>
             </div>
-            <div class="col-2"></div>
-            <div class="col-6">
+            <div class="col-0 col-lg-1"></div>
+            <div class="col-12 col-lg-6">
                 <div class="confirmation-message">
                     <div id = step-0>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/alert.png" 
