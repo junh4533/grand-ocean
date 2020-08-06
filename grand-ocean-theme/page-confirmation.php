0caef6d60480c39a249b234160b1ad3e1256193e
Jun <junh4533@gmail.com>
2020-08-06 16:40:06 -0400

    style order conf message

--- grand-ocean-theme/page-confirmation.php
+++ grand-ocean-theme/page-confirmation.php
@@ -24,44 +24,44 @@
                 </div>
             </div>
             <div class="col-xl-1"></div>
-            <div class="col-12 col-sm-9 col-md-6 col-xl-6 my-3 order-confirmation-container">
-                <div class="confirmation-message">
+            <div class="col-12 col-sm-9 col-md-6 col-xl-6 my-3 order-confirmation-container hv-center">
+                <div class="confirmation-message d-inline-block hv-center flex-column p-5">
                     <div id=step-0>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/alert.png" alt="alert icon"
                             width="50px" height="50px" />
-                        <h3>Reservation not yet completed!</h3>
+                        <h3 class="mb-5">Reservation not yet completed!</h3>
                     </div>
 
-                    <div id='step-1'>
+                    <div class="my-3 w-100" id='step-1'>
                         <h5 id='step-title'>Step 1: </h5>
                         <br>
                         <h5>Contact us via the below phone number/WeChat:</h5>
                         <br>
                         <br>
-                        <a class="nav-link" href="<?php echo get_site_url(); ?>/#footer">
-                            <i class="fab fa-weixin" id='icon'></i>
-                            <h5>&nbsp;&nbsp;&nbsp;Lisa307266</h5>
+                        <a class="d-inline mr-5" href="<?php echo get_site_url(); ?>/#footer">
+                            <i class="fab fa-weixin mr-3" id='icon'></i>
+                            <h5>Lisa307266</h5>
                         </a>
-                        <a class="nav-link" href="tel:347-532-0987">
-                            <i class="fas fa-phone" id='icon'></i>
-                            <h5>&nbsp;&nbsp;&nbsp;(347) 532-0987</h5>
+                        <a class="d-inline" href="tel:347-532-0987">
+                            <i class="fas fa-phone mr-3" id='icon'></i>
+                            <h5>(347) 532-0987</h5>
                         </a>
                     </div>
 
-                    <div id='step-2'>
+                    <div class="my-3 w-75" id='step-2'>
                         <h5 id='step-title'>Step 2: </h5>
                         <br>
                         <h5>Confirm your order with us!</h5>
                     </div>
 
-                    <div id='step-3'>
+                    <div class="my-3 w-75" id='step-3'>
                         <h5 id='step-title'>Step 3: </h5>
                         <br>
                         <h5>Wait for our delivery and enjoy the fresh seafood!</h5>
                     </div>
 
                     <div id='step-4'>
-                        <h6><em>* Failure of confirming will result in the cancellation of your order!</em></h6>
+                        <h6 class="mt-5"><em>* Failure of confirming will result in the cancellation of your order!</em></h6>
                     </div>
                 </div>
             </div>
