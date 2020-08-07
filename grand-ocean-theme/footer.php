484977a595338668bf6b9466d6d17ad872062c18
Ivy Tong <ivytong@Ivys-Air.lan>
2020-08-07 17:28:38 -0400

    responsiveness of footer

--- grand-ocean-theme/footer.php
+++ grand-ocean-theme/footer.php
@@ -2,9 +2,10 @@
 <footer id="footer">
     <div class="container-fluid py-5 px-sm-5">
         <div class="row">
-            <div class="col-6 col-xl-2 order-xl-2 px-sm-4 my-2">
+            <div class="col-12 col-sm-6 col-xl-2 order-xl-2 px-sm-4 my-5">
                 <p class="footer-heading">网页网址</p>
                 <a href="<?php echo get_site_url(); ?>">主页</a>
+                <a href="<?php echo get_site_url(); ?>/#about-us">我们的故事</a>
                 <a href="<?php echo get_site_url(); ?>/#products">今天的精选</a>
                 <a href="<?php echo get_site_url(); ?>/#products">联系我们</a>
                 <div class="my-3">   
@@ -14,7 +15,7 @@
                 </div>
             </div>
 
-            <div class="col-6 col-xl-2 order-xl-3 px-sm-4 my-2">
+            <div class="col-12 col-sm-6 col-xl-2 order-xl-3 px-sm-4 my-5">
                 <p class="footer-heading">联系方式</p>
                 <p>盛宴海鲜</p>
                 <p>35-20 College Point Blvd <br> Flushing, NY <br> 11354</p>
@@ -25,13 +26,13 @@
                     <p class="d-inline">微信: &nbsp; Lisa307266</p>
                 </div>
             </div>
-            <div class="col-6 col-xl-3 order-xl-1 px-sm-4 my-2">
+            <div class="col-8 col-sm-6 col-xl-3 order-xl-1 px-sm-4 my-5">
                 <img class="mb-3 img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/qr-code.jpg"
                     alt="WeChat QR Code">
                 <img class="img-fluid mx-auto my-3" id="footer-logo"
                     src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-white.png" alt="Logo White">
             </div>
-            <div class="col-6 col-xl-5 order-xl-4 text-right px-sm-4 my-2" id="footer-logo-section">
+            <div class="col-10 col-sm-6 col-xl-5 order-xl-4 text-right px-sm-4 my-5" id="footer-logo-section">
                 <a
                     class="text-center"
                     href="https://www.google.com/maps?q=35-20+College+Point+Blvd+Flushing,+NY+11354&rlz=1C1GCEA_enUS885US885&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiPk6vaivPqAhXsmOAKHVwWBdYQ_AUoAXoECA0QAw">
